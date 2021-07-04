4d153d874eb236240b448622c68beb18be298a10
David Kane <dave.kane@gmail.com>
2021-07-04 16:24:32 -0400

    transfered over

---
+++ final_project_clean_up.R
@@ -0,0 +1,21 @@
+# Clean up Final Project listing. Note the need to save the file as tab
+# delimitted since many students will have commas in their one-sentence
+# summaries.
+
+library(tidyverse)
+library(janitor) 
+
+x <- read_tsv("~/Downloads/final-projects-bootcamp-june-2021 - Sheet1.tsv", skip = 6) %>% 
+  janitor::clean_names() %>% 
+
+
+  mutate(output = paste0("[", name, "](", github_account, "): [",
+                         project_name, "](", project_url, "). ", 
+                         one_sentence_description_of_project)) %>% 
+  select(output) %>% 
+  sample_frac()
+
+
+
+writeLines(x$output, "projects.Rmd", sep = "  \n\n")
+
